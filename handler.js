bbc6a9b01cd6c912a45629c40722039f454ed84b
Manvir Singh <sehmbimanvir@gmail.com>
2021-02-09 20:20:18 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -5,13 +5,10 @@ var Jimp = require('jimp');
 module.exports.resize =  (event, context, callback) => {
 
   const s3Entity = event.Records[0].s3
-  console.log('S3 Entity', s3Entity)
 
   const BUCKET = s3Entity.bucket.name
   const HEIGHT = 100
   const WIDTH = 100
-
-  console.log('Bucket', BUCKET)
 
   var params = {
     Bucket: BUCKET,
